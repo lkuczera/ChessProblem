55c6287614030b8ad19c8b56f7e89d9f4fff1d93
lkuczera <l.kuczera@jextreme.pl>
2011-08-03 19:59:32 +0200

    additional tests

--- test/org/acidbits/CombinationsCheckerTest.scala
+++ test/org/acidbits/CombinationsCheckerTest.scala
@@ -1,32 +1,62 @@
 package org.acidbits
 import org.junit._
+import org.junit.Assert._
 import CombinationsChecker._
 import scala.collection.mutable.ArrayBuffer
 import Util._
 // FIXME add expected = IllegalArgumentException
 class CombinationsCheckerTest {
   
-  @Test
+//  @Test
   def testEmptyBoard() {
     val s = new CombinationsChecker
     val board = emptyBoard(0,0)
     s.countCombinations(board, King :: Nil)
   }
-  
-  @Test
+//  @Test(expected =  Exception.class)
   def testMorePiecesThanBoardSize() {
     val s = new CombinationsChecker
     val board = emptyBoard(3,3)
     s.countCombinations(board, King :: King :: King :: Nil)
   }
   
-  @Test
+//  @Test
   def testEmptyPieces()  {
     val s = new CombinationsChecker
     val board = emptyBoard(2,2)
     s.countCombinations(board, Nil)
   }
   
+//  @Test
+  def testQueenKing() {
+    val s = new CombinationsChecker
+    val board = emptyBoard(3,3)
+    val sol = s.countCombinations(board, Queen ::  King :: King :: Nil)
+    assertEquals(4, sol.size)
+  }
   
+//  @Test
+  def testThreeQueens() {
+    val s = new CombinationsChecker
+    val board = emptyBoard(4,4)
+    val sol = s.countCombinations(board, Queen ::  Queen :: Queen :: Nil)
+    sol.foreach(printBoard)
+    assertEquals(24, sol.size)
+  }
+  
+  @Test
+  def testQueen() {
+    val s = new CombinationsChecker
+    var board = emptyBoard(4,4)
+    s.putPiece(board, Queen, (2,1))
+    printBoard(board)
+    assertEquals(4, board.flatMap(_.filter(_ == Empty)).size)
+    
+    board = emptyBoard(4,4)
+    
+    s.putPiece(board, Queen, (1,1))
+    printBoard(board)
+    assertEquals(4, board.flatMap(_.filter(_ == Empty)).size)
+  }
   
 }
